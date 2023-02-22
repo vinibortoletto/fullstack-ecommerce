53aa85ce032e89ddaa67b8a19234269a676c4d32
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-22 15:27:07 -0300

    chore: configure sequelize

---
+++ backend/build/database/config/database.js
@@ -0,0 +1,14 @@
+"use strict";
+var config = {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    dialect: 'mysql',
+    dialectOptions: {
+        timezone: 'Z',
+    },
+    logging: false,
+};
+module.exports = config;
