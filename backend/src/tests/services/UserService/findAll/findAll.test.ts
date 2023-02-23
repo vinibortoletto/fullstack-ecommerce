a383954c1605419bfdf681d3d273f83ae587222d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-23 15:31:59 -0300

    test: find all users service

---
+++ backend/src/tests/services/UserService/findAll/findAll.test.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { Model } from 'sequelize';
+import Sinon from 'sinon';
+import UserService from '../../../../api/services/UserService';
+import * as mock from './findAll.mock';
+
+describe('UserService.findAll', function () {
+  afterEach(function () {
+    Sinon.restore();
+  });
+
+  it('should find all users', async function () {
+    Sinon.stub(Model, 'findAll').resolves(mock.findAll);
+    const userService = new UserService();
+    const result = await userService.findAll();
+    expect(result).to.deep.equal(mock.findAll);
+  });
+});
