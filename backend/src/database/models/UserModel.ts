47193cb9306b1f7eaa81bba34d3c3f001fd93e6e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-23 11:22:46 -0300

    feat: user model

---
+++ backend/src/database/models/UserModel.ts
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+export default class UserModel extends Model {
+  declare readonly id: number;
+  declare fullName: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  }
+);
