39941398148d9c01709f18e6e9370b62e30251d1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-23 19:27:31 -0300

    refactor: controller arrow function method

--- backend/src/api/controllers/UserController.ts
+++ backend/src/api/controllers/UserController.ts
@@ -4,12 +4,12 @@ import { NextFunction, Request, Response } from 'express';
 export default class UserController {
   constructor(private _userService: IUserService) {}
 
-  findAll = async (_req: Request, res: Response, next: NextFunction) => {
+  async findAll(_req: Request, res: Response, next: NextFunction) {
     try {
       const users = await this._userService.findAll();
       return res.status(200).json({ users });
     } catch (error) {
       next(error);
     }
-  };
+  }
 }
