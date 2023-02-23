d7f795f47df9a12142c0284b636dc6788d99cb19
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-23 17:38:51 -0300

    feat: app configuration

---
+++ backend/src/App.ts
@@ -0,0 +1,36 @@
+import 'express-async-errors';
+import express from 'express';
+import * as router from './api/routers';
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.initAuthHeader();
+    this.initRoutes();
+  }
+
+  private initAuthHeader(): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, DELETE, OPTIONS, PUT, PATCH'
+      );
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+
+  private initRoutes(): void {
+    this.app.use(router.user);
+  }
+
+  public start(PORT: number): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
