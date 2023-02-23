afd452b992a0b87972a9da50b93bbda3267c423f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-23 20:23:19 -0300

    fix: controller find all return

--- backend/src/api/controllers/UserController.ts
+++ backend/src/api/controllers/UserController.ts
@@ -7,7 +7,7 @@ export default class UserController {
   async findAll(_req: Request, res: Response, next: NextFunction) {
     try {
       const users = await this._userService.findAll();
-      return res.status(200).json({ users });
+      return res.status(200).json(users);
     } catch (error) {
       next(error);
     }
