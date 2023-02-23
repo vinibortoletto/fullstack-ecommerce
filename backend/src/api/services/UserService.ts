be883b2e4d1551e71b98b82766ab3a65979a03d3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-23 19:31:04 -0300

    fix: user service attribute

--- backend/src/api/services/UserService.ts
+++ backend/src/api/services/UserService.ts
@@ -1,7 +1,10 @@
+import { ModelStatic } from 'sequelize';
 import UserModel from '../../database/models/UserModel';
 import IUserService from '../interfaces/IUserService';
 
 export default class UserService implements IUserService {
+  protected userModel: ModelStatic<UserModel> = UserModel;
+
   public async findAll(): Promise<UserModel[]> {
     const users = await UserModel.findAll();
     return users;
