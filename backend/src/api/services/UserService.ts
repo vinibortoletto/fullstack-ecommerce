8c22201ae9d9465af108934b43bb011d7f449d42
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-25 17:14:00 -0300

    change: userModel attribute from protected to private

--- backend/src/api/services/UserService.ts
+++ backend/src/api/services/UserService.ts
@@ -3,10 +3,10 @@ import UserModel from '../../database/models/UserModel';
 import IUserService from '../interfaces/IUserService';
 
 export default class UserService implements IUserService {
-  protected userModel: ModelStatic<UserModel> = UserModel;
+  private _userModel: ModelStatic<UserModel> = UserModel;
 
   public async findAll(): Promise<UserModel[]> {
-    const users = await this.userModel.findAll();
+    const users = await this._userModel.findAll();
     return users;
   }
 }
