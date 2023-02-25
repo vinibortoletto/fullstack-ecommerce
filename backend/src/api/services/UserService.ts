0defc7ea742f4dfa875f7bd8f34a8982112075bc
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-25 20:09:22 -0300

    feat: UserService.findById method

--- backend/src/api/services/UserService.ts
+++ backend/src/api/services/UserService.ts
@@ -1,5 +1,6 @@
 import { ModelStatic } from 'sequelize';
 import UserModel from '../../database/models/UserModel';
+import NotFound from '../errors/NotFound';
 import IUserService from '../interfaces/IUserService';
 
 export default class UserService implements IUserService {
@@ -8,5 +9,11 @@ export default class UserService implements IUserService {
   public async findAll(): Promise<UserModel[]> {
     const users = await this._userModel.findAll();
     return users;
+  }
+
+  public async findById(id: number): Promise<UserModel> {
+    const user = await this._userModel.findByPk(id);
+    if (!user) throw new NotFound('User not found');
+    return user;
   }
 }
