12b20ff1b126703f1a84f1c31e4937e68e6cb155
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-09 19:19:07 -0300

    chore: user mock

--- backend/src/tests/mocks/index.ts
+++ backend/src/tests/mocks/index.ts
@@ -1,5 +1,6 @@
 import { findAll } from './findAll.mock';
 import { findById } from './findById.mock';
 import { newUser } from './create.mock';
+import * as usersMock from './usersMock';
 
-export { findAll, findById, newUser };
+export { findAll, findById, newUser, usersMock };
---
+++ backend/src/tests/mocks/usersMock.ts
@@ -0,0 +1,8 @@
+import UserModel from '../../database/models/UserModel';
+
+export const user: UserModel = {
+  id: 1,
+  fullName: 'Dalva Moso Bortoletto',
+  email: 'meu@email.com',
+  password: '123456',
+} as UserModel;
