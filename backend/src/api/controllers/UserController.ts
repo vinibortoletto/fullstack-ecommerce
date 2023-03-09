b49807700aa52a5d7825e0c32002d1c780c0a034
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-09 18:12:55 -0300

    feat: create user controller

--- backend/src/api/controllers/UserController.ts
+++ backend/src/api/controllers/UserController.ts
@@ -4,14 +4,20 @@ import { Request, Response } from 'express';
 export default class UserController {
   constructor(private _userService: IUserService) {}
 
-  async findAll(_req: Request, res: Response): Promise<Response | void> {
+  public async findAll(_req: Request, res: Response): Promise<Response | void> {
     const users = await this._userService.findAll();
     return res.status(200).json(users);
   }
 
-  async findById(req: Request, res: Response): Promise<Response | void> {
+  public async findById(req: Request, res: Response): Promise<Response | void> {
     const { id } = req.params;
     const user = await this._userService.findById(Number(id));
+    return res.status(200).json(user);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response | void> {
+    const newUser = req.body;
+    const user = await this._userService.create(newUser);
     return res.status(200).json(user);
   }
 }
