65a97b9a0ef2ece47f38ca66166b68107c7a4766
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-09 20:36:46 -0300

    feat: try/catch user controller

--- backend/src/api/controllers/UserController.ts
+++ backend/src/api/controllers/UserController.ts
@@ -1,23 +1,48 @@
 import IUserService from '../interfaces/IUserService';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { OK } from '../utils/httpErrorCodes';
 
 export default class UserController {
   constructor(private _userService: IUserService) {}
 
-  public async findAll(_req: Request, res: Response): Promise<Response | void> {
-    const users = await this._userService.findAll();
-    return res.status(200).json(users);
+  public async findAll(
+    _req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
+    try {
+      const users = await this._userService.findAll();
+      return res.status(OK).json(users);
+    } catch (error) {
+      next(error);
+    }
   }
 
-  public async findById(req: Request, res: Response): Promise<Response | void> {
+  public async findById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
     const { id } = req.params;
-    const user = await this._userService.findById(Number(id));
-    return res.status(200).json(user);
+    try {
+      const user = await this._userService.findById(Number(id));
+      return res.status(OK).json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 
-  public async create(req: Request, res: Response): Promise<Response | void> {
+  public async create(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> {
     const newUser = req.body;
-    const user = await this._userService.create(newUser);
-    return res.status(200).json(user);
+    try {
+      const user = await this._userService.create(newUser);
+      return res.status(OK).json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 }
