65de6b16120082262e92460ecda35ac152924e14
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-10 19:41:35 -0300

    chore: new user error messages

---
+++ backend/src/api/utils/errorMessages.ts
@@ -0,0 +1,18 @@
+export const fullNameErrorMessages = {
+  minLength: 'Nome deve ter, pelo menos, 3 caracteres',
+  empty: 'Nome não pode ser vazio',
+  required: 'Nome é um campo obrigatório',
+};
+
+export const emailErrorMessages = {
+  invalid: 'Email deve ser válido',
+  empty: 'Email não pode ser vazio',
+  required: 'Email é um campo obrigatório',
+};
+
+export const passwordErrorMessages = {
+  minLength: 'Senha deve ter, pelo menos, 8 caracteres',
+  maxLength: 'Senha deve ter, no máximo, 100 caracteres',
+  empty: 'Senha não pode ser vazio',
+  required: 'Senha é um campo obrigatório',
+};
