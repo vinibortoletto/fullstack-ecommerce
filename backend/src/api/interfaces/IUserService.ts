03b933ca5afe1435ce92d340d0e397c28ba18f16
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-10 23:55:27 -0300

    feat: user service interface

--- backend/src/api/interfaces/IUserService.ts
+++ backend/src/api/interfaces/IUserService.ts
@@ -1,7 +1,9 @@
 import UserModel from '../../database/models/UserModel';
+import ILogin from './ILogin';
 
 export default interface IUserService {
   findAll(): Promise<UserModel[]>;
   findById(id: number): Promise<UserModel>;
   create(newUser: UserModel): Promise<UserModel>;
+  login(login: ILogin): Promise<string>;
 }
