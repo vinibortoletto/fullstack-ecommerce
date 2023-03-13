7f77bab093aedb25b95e0be3aabafc52d847e3ec
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-13 20:33:34 -0300

    chore: user service interface

--- backend/src/api/interfaces/IUserService.ts
+++ backend/src/api/interfaces/IUserService.ts
@@ -1,9 +1,10 @@
 import UserModel from '../../database/models/UserModel';
 import ILogin from './ILogin';
+import IToken from './IToken';
 
 export default interface IUserService {
   findAll(): Promise<UserModel[]>;
   findById(id: number): Promise<UserModel>;
   create(newUser: UserModel): Promise<UserModel>;
-  login(login: ILogin): Promise<string>;
+  login(login: ILogin): Promise<IToken>;
 }
