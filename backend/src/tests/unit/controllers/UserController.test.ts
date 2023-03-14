ced585a2bee29121be59a16182977a8d55522096
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-03-13 21:15:53 -0300

    refactor: user controller tests

--- backend/src/tests/unit/controllers/UserController.test.ts
+++ backend/src/tests/unit/controllers/UserController.test.ts
@@ -1,14 +1,26 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { Request, Response } from 'express';
 import sinon from 'sinon';
-import UserController from '../../../api/controllers/UserController';
-import UserService from '../../../api/services/UserService';
-import { CREATED, OK, UNAUTHORIZED } from '../../../api/utils/httpStatusCodes';
+import sinonChai from 'sinon-chai';
+
+import { UserController } from '../../../api/controllers';
+import { UserService } from '../../../api/services';
+import { CREATED, OK } from '../../../api/utils/httpStatusCodes';
 import { usersMock } from '../../mocks';
+
+chai.use(sinonChai);
 
 describe('Unit tests for UserController', function () {
   const userService = new UserService();
   const userController = new UserController(userService);
+  let res = {} as Response;
+  let req = {} as Request;
+  let next = sinon.stub();
+
+  beforeEach(function () {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
 
   afterEach(function () {
     sinon.restore();
@@ -16,77 +28,47 @@ describe('Unit tests for UserController', function () {
 
   describe('findAll method', function () {
     it('should find all users', async function () {
-      const res = {} as Response;
-      const req = {} as Request;
-      const next = sinon.stub();
-
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns(res);
-
       sinon.stub(userService, 'findAll').resolves(usersMock.userList);
       await userController.findAll(req, res, next);
 
-      expect((res.status as sinon.SinonStub).calledWith(OK)).to.be.true;
-      expect((res.json as sinon.SinonStub).calledWith(usersMock.userList)).to.be
-        .true;
+      expect(res.status).to.have.been.calledWith(OK);
+      expect(res.json).to.have.been.calledWith(usersMock.userList);
     });
   });
 
   describe('findById method', function () {
     it('should find user by id', async function () {
-      const res = {} as Response;
-      const req = { params: { id: 1 } } as unknown as Request;
-      const next = sinon.stub();
-
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns(res);
-
+      req = { params: { id: 1 } } as unknown as Request;
       sinon.stub(userService, 'findById').resolves(usersMock.user);
+
       await userController.findById(req, res, next);
 
-      expect((res.status as sinon.SinonStub).calledWith(OK)).to.be.true;
-      expect((res.json as sinon.SinonStub).calledWith(usersMock.user)).to.be
-        .true;
+      expect(res.status).to.have.been.calledWith(OK);
+      expect(res.json).to.have.been.calledWith(usersMock.user);
     });
   });
 
   describe('create method', function () {
     it('should create user', async function () {
-      const res = {} as Response;
-      const req = {
-        body: { ...usersMock.newUserBody },
-      } as unknown as Request;
-      const next = sinon.stub();
-
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns(res);
-
+      req.body = usersMock.newUserBody as unknown as Request;
       sinon.stub(userService, 'create').resolves(usersMock.newUser);
+
       await userController.create(req, res, next);
 
-      expect((res.status as sinon.SinonStub).calledWith(CREATED)).to.be.true;
-      expect((res.json as sinon.SinonStub).calledWith(usersMock.newUser)).to.be
-        .true;
+      expect(res.status).to.have.been.calledWith(CREATED);
+      expect(res.json).to.have.been.calledWith(usersMock.newUser);
     });
   });
 
   describe('login method', function () {
     it('should be able to login', async function () {
-      const res = {} as Response;
-      const req = {
-        body: { ...usersMock.login },
-      } as unknown as Request;
-      const next = sinon.stub();
-
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns(res);
-
+      req.body = usersMock.login as unknown as Request;
       sinon.stub(userService, 'login').resolves({ token: 'token' });
+
       await userController.login(req, res, next);
 
-      expect((res.status as sinon.SinonStub).calledWith(OK)).to.be.true;
-      expect((res.json as sinon.SinonStub).calledWith({ token: 'token' })).to.be
-        .true;
+      expect(res.status).to.have.been.calledWith(OK);
+      expect(res.json).to.have.been.calledWith({ token: 'token' });
     });
   });
 });
